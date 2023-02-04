e364580bb7cafc58ce67acdada2996bfb39f6707
yennhi2602 <116791512+yennhi2602@users.noreply.github.com>
2023-02-04 08:03:06 +0700

    Add files via upload

---
+++ BT14yc2.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+
+///yc2
+
+
+long  tinhGiaithua(int n)
+{
+	if  (n > 0){
+		return n* tinhGiaithua (n - 1);
+	}else {
+		return 1;
+	}
+}
+
+
+int main()
+{
+	int n;
+	printf("\n Nhap so nguyen duong n: ");
+	scanf("%d",&n);
+	printf("Giai thua cua %d la : %d \n", n , tinhGiaithua(n));
+}
+
