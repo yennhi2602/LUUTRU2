c3bfddea96772dda7641c3cd79573a3d3cb69e4a
yennhi2602 <116791512+yennhi2602@users.noreply.github.com>
2023-05-21 09:59:30 +0700

    Add files via upload

---
+++ 2602.cpp
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<conio.h>
+
+    void nhapmang(int a[],int n)
+    {
+	   for(int i=0;i<=n;i++)
+    	{
+		printf("\n\n\tNhap phan tu thu a[%d]=",i);
+		scanf("%d",&a[i]);
+     	}
+    }
+    void xuatmang(int a[],int n)
+    {
+       printf("\n\t mang : ");
+        {
+    	for(int i=0;i<=n;i++)
+    	    printf("  %d  ",a[i]);
+    	}
+    }
+       void hv ( int &a, int &b)
+  {
+    int temp = a;
+    a = b;
+    b = temp;
+
+  }
+    void BubleSort ( int a[], int n)   // noi bot
+    {
+    	int i, j;
+    	for ( i=0; i<n-1;i++)
+    	{
+    	
+    	   for(j=n-1; j>i;j--)
+    	   {
+    	   
+    	       if(a[j]<a[j-1])
+    	       {
+    	       		hv(a[j],a[j-1]);
+			   }
+			   
+    	    }
+  		}	 
+	}
+int linearsearch( int a[], int n, int x)
+{
+   	printf("\n Nhap gia tri can tim: ");
+   	scanf("%d",&x);
+    	int i=0;
+    	a[n]=x;
+ 	while (a[i]!=x)
+    	i++;
+	if (i==n)
+    	return -1;
+    else
+	    return i;
+}
+
+
+void Inserttionsort ( int a[], int n)
+{
+	int pos,i;
+	int x;
+	for(int i=1;i<n;i++)
+	{
+		x=a[i];
+		pos=i-1;
+		while((pos >= 0)&&(a[pos]>x))
+		{
+			a[pos+1]=a[pos];
+			pos--;
+		}
+		a[pos+1]=x;
+	}
+}
+void ThemPhanTu(int a[], int index, int PhanTu, int &n){
+    
+    if(index <= 0){
+        index = 0;
+    }
+    
+    if(index >= n){
+        index = n;
+    }
+    
+    for(int i = n; i > index; i--){
+        
+        a[i] = a[i-1];
+    }
+    
+    a[index] = PhanTu;
+   
+    n++;
+}
+  int main()
+ {
+ 	int a[1000]; int n;int x;
+ 	printf("\n Nhap n: ");
+ 	scanf("%d",&n);
+ 	nhapmang(a,n);
+ 	xuatmang(a,n);
+ 	BubleSort(a,n);
+ 	printf("\n Mang duoc sap xep la: ");
+ 	xuatmang(a,n);
+ 	int result=linearsearch(a,n,x);
+ 	if( result==-1)
+ 	printf(" Khong tim thay phan tu trong mang",x);
+ 	else
+ 	printf(" So can tim nam o vi tri so %d", result);
+	int PhanTu;
+	n++;
+    printf(" \n NHAP PHAN TU CAN THEM:  ");
+    scanf("%d",&PhanTu);
+    a[n]=PhanTu;
+    printf("\n Mang duoc sap xep la: ");
+    xuatmang(a,n);
+ 	return 0; 
+	 
+}
